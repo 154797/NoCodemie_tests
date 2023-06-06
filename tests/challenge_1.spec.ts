b57624799486046a9f52edc9a59441680640835e
Ehab <56022276+154797@users.noreply.github.com>
2023-06-06 14:17:43 +0200

    Update challenge_1.spec.ts

--- tests/challenge_1.spec.ts
+++ tests/challenge_1.spec.ts
@@ -8,12 +8,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto(newPageUrl);
 });
 
-test(`1.1 Create a new blank page and name it ${newPageTitle}`, async ({
-  page,
-}) => {
-  const rootElement = page.locator("id=app");
-  await expect(rootElement, "page is create and compiled").toBeDefined();
-});
 
 test(`1.2 Drag a ‘1 Column’ component onto your page`, async ({ page }) => {
   const row = await page.locator(`[data-component=Row]`).nth(0);
@@ -24,4 +18,11 @@ test(`1.2 Drag a ‘1 Column’ component onto your page`, async ({ page }) => {
     "column should be in document"
   ).toBeVisible();
   await page.waitForTimeout(2000);
+});
+
+test(`1.1 Create a new blank page and name it ${newPageTitle}`, async ({
+  page,
+}) => {
+  const rootElement = page.locator("id=app");
+  await expect(rootElement, "page is create and compiled").toBeDefined();
 });
