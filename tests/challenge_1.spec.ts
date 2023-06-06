2c072d66ae15215c4001649e69cbdad2c27f75aa
Ehab <56022276+154797@users.noreply.github.com>
2023-06-06 14:15:28 +0200

    Update challenge_1.spec.ts

--- tests/challenge_1.spec.ts
+++ tests/challenge_1.spec.ts
@@ -25,32 +25,3 @@ test(`1.2 Drag a ‘1 Column’ component onto your page`, async ({ page }) => {
   ).toBeVisible();
   await page.waitForTimeout(2000);
 });
-
-test(`1.3 Drag a ‘Text’ component into your ‘1 Column’ component`, async ({
-  page,
-}) => {
-  const column = page.locator(`[data-component=Column]`).nth(0);
-  await expect(column, "column should be in document").toBeVisible();
-
-  await expect(
-    column.locator(`[data-component=Text]`).nth(0),
-    "text should be in document"
-  ).toBeVisible();
-  await page.waitForTimeout(2000);
-});
-
-const textContent = `My first page`;
-
-test(`1.4 Have the ‘Text’ component display the following text: ${textContent}`, async ({
-  page,
-}) => {
-  const column = page.locator(`[data-component=Column]`).nth(0);
-  await expect(column, "coulmn should be in document").toBeVisible();
-  const text = await column
-    .locator(`[data-component=Text]`)
-    .nth(0)
-    .textContent(); 
-
-  await expect(text, `text should be: `).toBe(textContent);
-  await page.waitForTimeout(2000);
-});
