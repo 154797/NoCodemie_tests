48ac0a6a42276b1d336e269444506c832aa590ba
Ehab <56022276+154797@users.noreply.github.com>
2023-06-08 09:00:55 +0200

    Update challenge_1.spec.ts

--- tests/challenge_1.spec.ts
+++ tests/challenge_1.spec.ts
@@ -20,40 +20,18 @@ test(`1.1 Create a new blank page and name it ${newPageTitle}`, async ({
 
 test(`1.2 Drag a ‘1 Column’ component onto your page`, async ({ page }) => {
   const row = await page.locator(`[data-component="Row"]`).nth(0);
-  await expect(row, "row should be in document").toBeVisible();
-
-  await expect(
-    row.locator(`[data-component="Column"]`).nth(0),
-    "column should be in document"
-  ).toBeVisible();
-  await page.waitForTimeout(3000);
+  await expect(row).toBeVisible();
+  await page.waitForTimeout(2000);
+  const column = page.locator(`[data-component="Column"]`).nth(0);
+  await expect(column).toBeVisible();
+  await page.waitForTimeout(2000);
 });
+
 test(`1.3 Drag a ‘Text’ component into your ‘1 Column’ component`, async ({
   page,
 }) => {
-  const column = page.locator(`[data-component="Column"]`).nth(0);
-  await expect(column, "column should be in document").toBeVisible();
-
-  await expect(
-    column.locator(`[data-component="Text"]`).nth(0),
-    "text should be in document"
-  ).toBeVisible();
+  const text = page.locator(`[data-component="Text"]`).nth(0);
+  await expect(text).toBeVisible();
   await page.waitForTimeout(3000);
 });
 
-const textContent = `My first page`;
-
-test(`1.4 Have the ‘Text’ component display the following text: ${textContent}`, async ({
-  page,
-}) => {
-  const column = page.locator(`[data-component="Column"]`).nth(0);
-  await expect(column, "coulmn should be in document").toBeVisible();
-  const text = await column
-    .locator(`[data-component="Text"]`)
-    .nth(0)
-    .textContent();
-
-  await expect(text, `text should be: `).toBe(textContent);
-  await page.waitForTimeout(3000);
-
-});
